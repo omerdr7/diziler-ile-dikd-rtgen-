4620829706a61dcf57b31dc273df4ac2a2ae5b16
omerdr7 <omerfarukdere32@gmail.com>
2022-12-25 16:08:42 +0300

    diziler ile dikdörtgen
    diziler ile dikdörtgen

---
+++ diziler2/diziler2/diziler2.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+
+class Hesapla {
+
+public:
+	int genislik;
+	int yukseklik;
+	int alan;
+	void giris() {
+		std::cout << "Yukseklik Gir" << std::endl;
+		std::cin >> yukseklik;
+
+		std::cout << "Genislik Gir" << std::endl;
+		std::cin >> genislik;
+}
+	void hesapla() {
+		alan = yukseklik * genislik;
+		std::cout << alan;
+
+	}
+};
+
+int main()
+{
+
+	Hesapla hesap;
+	hesap.giris();
+	hesap.hesapla();
+
+
+
+
+}
+
+
